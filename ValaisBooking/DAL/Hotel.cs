0de5633ddce977993c083f34e0848e5482994c20
David Crittin <david.crittin@students.hevs.ch>
2019-10-11 14:38:24 +0200

    Created appsettings.json Added Hotel & HotelsDB on DAL

---
+++ ValaisBooking/DAL/Hotel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL
+{
+    public class Hotel
+    {
+
+        public int IdHotel { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string Location { get; set; }
+
+        public string Category { get; set; }
+
+        public bool HasWifi { get; set; }
+
+        public bool HasParking { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Email { get; set; }
+
+        public string Website { get; set; }
+
+        public override string ToString()
+        {
+            return $"{IdHotel}|{Name}|{Description}|{Location}|{Category}|{HasWifi}|{HasParking}|{Phone}|{Email}|{Website}";
+        }
+    }
+
+}
